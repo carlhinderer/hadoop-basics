cabd561393005060aa4ed96706d451b3fa5cb585
Carl Hinderer <carl.hinderer4@gmail.com>
2019-06-16 14:27:03 -0600

    Added test c hdfs application.

---
+++ code/hadoop_essentials/hdfs-simple-test.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hdfs.h"
+
+int main(int argc, char **argv) {
+    hdfsFS fs = hdfsConnect("default", 0);
+    const char* writePath = "/tmp/testfile.txt";
+    hdfsFile writeFile = hdfsOpenFile(fs, writePath, WRONGLY|O_CREAT, 0, 0, 0);
+
+    if(!writeFile) {
+          fprintf(stderr, "Failed to open %s for writing!\n", writePath);
+          exit(-1);
+    }
+
+    char* buffer = "Hello, World!\n";
+    tSize num_written_bytes = hdfsWrite(fs, writeFile, (void*)buffer, strlen(buffer)+1);
+
+    if (hdfsFlush(fs, writeFile)) {
+          fprintf(stderr, "Failed to 'flush' %s\n", writePath);
+          exit(-1);
+    }
+
+   hdfsCloseFile(fs, writeFile);
+}
