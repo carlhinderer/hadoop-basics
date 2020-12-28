0ef71d51fa7a3c2968ced5b536956939b8fa48f8
Carl Hinderer <carl.hinderer4@gmail.com>
2020-12-27 20:36:49 -0700

    Added example of reading from HDFS files using the FileSystem API.

---
+++ hadoop_definitive_guide/code/ch02/FileSystemCat.java
@@ -0,0 +1,17 @@
+public class FileSystemCat {
+    
+    public static void main(String[]args) throws Exception {
+        String uri = args[0];
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(uri),conf);
+        InputStream in = null;
+
+        try {
+            in = fs.open(newPath(uri));
+            IOUtils.copyBytes(in,System.out,4096,false);
+        }
+        finally {
+            IOUtils.closeStream(in);
+        }
+    }
+}
